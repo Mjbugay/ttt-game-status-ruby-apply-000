ed1bf653fbbcf55a0bc6c1ef04fd8a13e98d4db4
energetic-certificate-0804 <mjbugay@yahoo.com>
2020-08-28 17:38:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,12 +24,12 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
     if ((position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")) && !position_taken?(board, win_index_1)
-      return win_combination.Array
-    else
-      end
-    return nil
-  end
-end  
+      return win_combination
+#     else
+#       end
+#     return nil
+#   end
+# end  
       
       
   
