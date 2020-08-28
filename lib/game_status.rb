8113c014c764ccd3c9f3966a219d02dc46db60b6
energetic-certificate-0804 <mjbugay@yahoo.com>
2020-08-28 17:22:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,10 +26,11 @@ def won?(board)
     if ((position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O"))
       return win_combination
     else
-      return nil
-    end  
+      end
+    return FALSE  
   end
-end  
+ end  
+end 
       
       
   
